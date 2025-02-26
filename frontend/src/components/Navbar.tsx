40618564dbb6dbd6fa4d41758800ebb462e44a7b
nico-private-dev <nforget82@gmail.com>
2025-02-26 19:12:36 +0100

    changement dropdown navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,13 +4,49 @@ import { useAuth } from '../context/AuthContext';
 import {
   Dialog,
   DialogPanel,
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Popover,
+  PopoverButton,
   PopoverGroup,
+  PopoverPanel,
 } from '@headlessui/react';
 import {
   Bars3Icon,
   XMarkIcon,
-  UserCircleIcon
+  UserCircleIcon,
+  ChevronDownIcon,
 } from '@heroicons/react/24/outline';
+import { BsLungs, BsEmojiSmile, BsGraphUp, BsPerson } from 'react-icons/bs';
+
+
+const tools = [
+  {
+    name: 'Exercice de respiration',
+    description: 'Apprenez à gérer votre stress grâce à des exercices de respiration guidés',
+    href: '/exercice-respiration',
+    icon: BsLungs, // Remplacez par l'icône appropriée
+  },
+  {
+    name: 'Diagnostic',
+    description: 'Évaluez votre niveau de stress',
+    href: '#',
+    icon: BsGraphUp, // Remplacez par l'icône appropriée
+  },
+  {
+    name: 'Tracker d\'émotions',
+    description: 'Suivez vos émotions au quotidien',
+    href: '#',
+    icon: BsEmojiSmile, // Remplacez par l'icône appropriée
+  },
+  {
+    name: 'Activités détentes',
+    description: 'Gérez votre stress grâce à des activités relaxantes',
+    href: '#',
+    icon: BsPerson, // Remplacez par l'icône appropriée
+  }
+];
 
 const Navbar: React.FC = () => {
   const { isAuthenticated, logout } = useAuth();
@@ -40,33 +76,57 @@ const Navbar: React.FC = () => {
           </button>
         </div>
         <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+          <Link to="/about" className="text-sm font-semibold leading-6 text-gray-900">
+            À propos
+          </Link>
+          <Popover className="relative flex">
+            <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold text-gray-900">
+              Les outils
+              <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none text-gray-400" />
+            </PopoverButton>
 
-          <Link to="/exercice-respiration" className="text-sm font-semibold leading-6 text-gray-900">
-            Exercice de respiration
-          </Link>
-          <Link to="/" className="text-sm font-semibold leading-6 text-gray-900">
-            Diagnostic
-          </Link>
-          <Link to="/" className="text-sm font-semibold leading-6 text-gray-900">
-            Tracker d'émotions
-          </Link>
-          <Link to="/" className="text-sm font-semibold leading-6 text-gray-900">
+            <PopoverPanel
+              transition
+              className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white ring-1 shadow-lg ring-gray-900/5 transition data-closed:translate-y-1 data-closed:opacity-0 data-enter:duration-200 data-enter:ease-out data-leave:duration-150 data-leave:ease-in"
+            >
+              <div className="p-4">
+                {tools.map((item) => (
+                  <div
+                    key={item.name}
+                    className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm hover:bg-gray-50"
+                  >
+                    <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                      <item.icon aria-hidden="true" className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" />
+                    </div>
+                    <div className="flex-auto">
+                      <Link to={item.href} className="block font-semibold text-gray-900">
+                        {item.name}
+                        <span className="absolute inset-0" />
+                      </Link>
+                      <p className="mt-1 text-gray-600">{item.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </PopoverPanel>
+          </Popover>
+          <Link to="/information" className="text-sm font-semibold leading-6 text-gray-900">
             Informations
           </Link>
         </PopoverGroup>
         <div className="hidden lg:flex lg:justify-end gap-4">
           {isAuthenticated ? (
-                <>
-                <Link 
-                  to="/mon-compte" 
-                  className="inline-flex items-center text-sm font-semibold leading-6 text-primary hover:text-secondary transition duration-300 ease"
-                >
-                  <UserCircleIcon className="h-6 w-6" />
-                </Link>
-                <button onClick={logout} className="text-sm font-semibold leading-6 text-gray-900 bg-secondary py-2 px-6 rounded border-2 border-secondary text-black hover:bg-transparent hover:border-secondary transition duration-300 ease">
-                  Se déconnecter <span aria-hidden="true">&rarr;</span>
-                </button>
-              </>
+            <>
+              <Link 
+                to="/mon-compte" 
+                className="inline-flex items-center text-sm font-semibold leading-6 text-primary hover:text-secondary transition duration-300 ease"
+              >
+                <UserCircleIcon className="h-6 w-6" />
+              </Link>
+              <button onClick={logout} className="text-sm font-semibold leading-6 text-gray-900 bg-secondary py-2 px-6 rounded border-2 border-secondary text-black hover:bg-transparent hover:border-secondary transition duration-300 ease">
+                Se déconnecter <span aria-hidden="true">&rarr;</span>
+              </button>
+            </>
           ) : (
             <>
               <Link to="/register" className="text-sm font-semibold leading-6 text-gray-900 bg-primary py-2 px-6 rounded border-2 border-primary text-white hover:bg-transparent hover:border-primary hover:text-primary transition duration-300 ease">
@@ -103,41 +163,53 @@ const Navbar: React.FC = () => {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
+                <Disclosure as="div" className="-mx-3">
+                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base font-semibold text-gray-900 hover:bg-gray-50">
+                    Les outils
+                    <ChevronDownIcon aria-hidden="true" className="h-5 w-5 flex-none group-data-open:rotate-180" />
+                  </DisclosureButton>
+                  <DisclosurePanel className="mt-2 space-y-2">
+                    {tools.map((item) => (
+                      <DisclosureButton
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        className="block rounded-lg py-2 pr-3 pl-6 text-sm font-semibold text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </DisclosureButton>
+                    ))}
+                  </DisclosurePanel>
+                </Disclosure>
                 <Link
-                  to="/exercice-respiration"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  to="/about"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50"
                 >
-                  Exercice de respiration
+                  À propos
                 </Link>
                 <Link
-                  to="/"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  to="/information"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold text-gray-900 hover:bg-gray-50"
                 >
-                  Diagnostic
-                </Link>
-                <Link
-                  to="/"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Tracker
+                  Informations
                 </Link>
               </div>
               <div className="py-6">
                 {isAuthenticated ? (
                   <button onClick={logout} className="text-sm font-semibold leading-6 text-gray-900 bg-secondary py-2 px-6 rounded border-2 border-secondary text-black hover:bg-transparent hover:border-secondary transition duration-300 ease">
-                  Se déconnecter <span aria-hidden="true">&rarr;</span>
-                </button>
+                    Se déconnecter <span aria-hidden="true">&rarr;</span>
+                  </button>
                 ) : (
                   <>
-                  <div className='flex gap-4'>
-                    <Link to="/register" className="text-sm font-semibold leading-6 text-gray-900 bg-primary py-2 px-6 rounded border-2 border-primary text-white hover:bg-transparent hover:border-primary hover:text-primary transition duration-300 ease">
-                      S'inscrire
-                    </Link>
-                    <Link to="/login" className="text-sm font-semibold leading-6 text-gray-900 bg-secondary py-2 px-6 rounded border-2 border-secondary text-black hover:bg-transparent hover:border-secondary transition duration-300 ease">
-                      Se connecter <span aria-hidden="true">&rarr;</span>
-                    </Link>
-                  </div>
-                </>
+                    <div className='flex gap-4'>
+                      <Link to="/register" className="text-sm font-semibold leading-6 text-gray-900 bg-primary py-2 px-6 rounded border-2 border-primary text-white hover:bg-transparent hover:border-primary hover:text-primary transition duration-300 ease">
+                        S'inscrire
+                      </Link>
+                      <Link to="/login" className="text-sm font-semibold leading-6 text-gray-900 bg-secondary py-2 px-6 rounded border-2 border-secondary text-black hover:bg-transparent hover:border-secondary transition duration-300 ease">
+                        Se connecter <span aria-hidden="true">&rarr;</span>
+                      </Link>
+                    </div>
+                  </>
                 )}
               </div>
             </div>
