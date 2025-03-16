8c81dab7e81dd79c6ca467680c151b0544482903
nico-private-dev <nforget82@gmail.com>
2025-03-16 19:45:18 +0100

    Ajout commentaires utils

--- backend/utils/generateToken.ts
+++ backend/utils/generateToken.ts
@@ -1,9 +1,11 @@
 import jwt from 'jsonwebtoken';
 
+// Fonction pour générer un token d'authentification
 export const generateAccessToken = (id: string) => {
   return jwt.sign({ _id: id }, process.env.JWT_SECRET!, { expiresIn: '15m' });
 };
 
+// Fonction pour générer un token de rafraichissement
 export const generateRefreshToken = (id: string) => {
   return jwt.sign({ _id: id }, process.env.JWT_REFRESH_SECRET!, { expiresIn: '7d' });
 };
