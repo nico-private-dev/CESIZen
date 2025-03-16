7bffa2ff1257ac12ac1bc3798e668c75540499ba
nico-private-dev <nforget82@gmail.com>
2025-03-16 19:46:46 +0100

    Ajout commentaires initRoles

--- backend/initRoles.ts
+++ backend/initRoles.ts
@@ -2,11 +2,14 @@ import dotenv from 'dotenv';
 import RoleModel from './models/roleModels';
 import connectDB from './config/db';
 
+// Charger les variables d'environnement
 dotenv.config();
 
+// Fonction pour initialiser les rôles
 const initRoles = async () => {
   await connectDB();
 
+  // Création des rôles
   const roles = [
     { name: 'admin' },
     { name: 'user' },
@@ -14,10 +17,10 @@ const initRoles = async () => {
 
   try {
     for (const role of roles) {
+      // Création du rôle
       const newRole = new RoleModel(role);
       await newRole.save();
     }
-    console.log('Roles initialized');
     process.exit();
   } catch (error) {
     process.exit(1);
