e699d82a99dccbbe361d20966284f485cc055e52
nico-private-dev <nforget82@gmail.com>
2025-03-23 16:24:01 +0100

    modified configuration cors

--- backend/server.ts
+++ backend/server.ts
@@ -24,7 +24,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: ['http://localhost:3000', 'http://192.168.1.120:3000'], 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'] 
